a1eaa85f2e426faa875fee923b6959cacd3a65e0
Jorge Perez <jorgeaperez8@gmail.com>
2023-01-20 17:10:07 -0400

    Primeiro commit - projeto criado

---
+++ src/test/java/com/scasoftware/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scasoftware.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
