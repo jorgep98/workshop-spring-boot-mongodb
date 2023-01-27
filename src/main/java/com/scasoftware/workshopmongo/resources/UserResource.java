663631446384fcaca259db64d2e00e6a06f71909
Jorge Perez <jorgeaperez8@gmail.com>
2023-01-27 15:47:43 -0400

    Inserção de usuário com POST

--- src/main/java/com/scasoftware/workshopmongo/resources/UserResource.java
+++ src/main/java/com/scasoftware/workshopmongo/resources/UserResource.java
@@ -1,15 +1,18 @@
 package com.scasoftware.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.scasoftware.workshopmongo.domain.User;
 import com.scasoftware.workshopmongo.dto.UserDTO;
@@ -36,6 +39,15 @@ public class UserResource {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
 		
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
+		
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 		
 	}
 
