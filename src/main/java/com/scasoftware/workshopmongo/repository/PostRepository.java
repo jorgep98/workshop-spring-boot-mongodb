ba89e706327944fbdd3b071a2412683188021e0a
Jorge Perez <jorgeaperez8@gmail.com>
2023-01-30 12:31:33 -0400

    Criando entity Post com User aninhado

---
+++ src/main/java/com/scasoftware/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,13 @@
+package com.scasoftware.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.scasoftware.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+	
+	
+
+}
